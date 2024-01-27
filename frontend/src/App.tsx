ada72727f50e68d5443f9e63467a7a1fc46a4c00
Anderson Zobel <87586643+Anderson-Zobel@users.noreply.github.com>
2024-01-27 00:27:41 -0300

    feat: component SideBar.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Schedulings from "./pages/Schedulings/Schedulings";
 import Clients from "./pages/Clients/Clients";
 import Pets from "./pages/Pets/Pets";
 import NoMatch from "./pages/NoMatch/NoMatch";
+import Sidebar from "./components/SideBar";
 
 function App() {
 
   return (
     <>
+
+        <Sidebar/>
         <Routes>
             <Route path="/" element={<Schedulings />}/>
             <Route path="/clientes" element={<Clients />}/>
